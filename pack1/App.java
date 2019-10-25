908b41e577f560841cc2ebd2311db04f7a24e34a
cerritej <55205133+cerritej@users.noreply.github.com>
2019-10-24 21:53:11 -0400

    starting to intitialize GUI

--- pack1/App.java
+++ pack1/App.java
@@ -7,10 +7,10 @@ public class App {
 	public static void main(String[]args) {
 		
 		SwingUtilities.invokeLater(new Runnable() {
-
 			
 			public void run() {
-				new MainFrame();					
+				new MainFrame();	
+				
 			}
 			
 		});
--- pack1/MainFrame.java
+++ pack1/MainFrame.java
@@ -1,14 +1,43 @@
 package pack1;
 import javax.swing.*;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+
 public class MainFrame extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	
+	JButton startBtn;
+	JButton loadBtn;
+	JButton optnBtn;
+	JButton secretBtn;
+	GridBagConstraints gbc = new GridBagConstraints();
+
 	public MainFrame() {
+		
 		super("Soul Survivor");
+		
+		setLayout(new GridBagLayout());
 		
 	
 		setSize(1920, 1080);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setUndecorated(true);
 		setVisible(true);
+		
+		startBtn = new JButton("");
+		add(startBtn);
+		
+		loadBtn = new JButton("");
+		add(loadBtn);
+		
+		optnBtn = new JButton("");
+		add(optnBtn);
+		
+		optnBtn = new JButton("");
+		add(secretBtn);
+		
 	}
 }
