c2b20e1def6e4f3b4dafefa2f2adb27e0d40dab0
Kyle Jacobson <jacokyle@mail.gvsu.edu>
2019-11-15 17:22:10 -0500

    Major improvements to the GUI, functional start button.

--- src/gamePackage/primaryGUI.java
+++ src/gamePackage/primaryGUI.java
@@ -16,6 +16,11 @@ import java.io.IOException;
  ***********************************************************************/
 class primaryGUI extends JFrame {
 
+	private JButton startBtn = new JButton("");
+	private JButton loadBtn = new JButton("");
+	private JButton optionBtn = new JButton("");
+	private JButton secretBtn = new JButton("");
+
 	private static final long serialVersionUID = 1L;
 
 	/******************************************************************
@@ -27,55 +32,6 @@ class primaryGUI extends JFrame {
 	primaryGUI() throws IOException, UnsupportedAudioFileException, LineUnavailableException {
 		super("Soul Survivor");
 
-		// Instantiates panels for the mainMenu.
-		JLayeredPane backgroundPane = new JLayeredPane();
-		JLayeredPane mainMenuButtonsPane = new JLayeredPane();
-
-		// Creates a layout manager to handle the organization.
-		OverlayLayout overlayBackgroundPanel = new OverlayLayout(backgroundPane);
-
-		// Sets the layouts for the  panels.
-		backgroundPane.setLayout(overlayBackgroundPanel);
-		mainMenuButtonsPane.setLayout(null);
-
-		// Creates a background for the GUI using the assigned photo.
-		JLabel mainMenu = new JLabel("");
-		mainMenu.setMaximumSize(new Dimension(1920, 1080));
-		mainMenu.setIcon(new ImageIcon("resources\\ui\\Main Menu.jpg"));
-		backgroundPane.add(mainMenu);
-
-		// Creates and aligns the start button for the main menu.
-		JButton startBtn = new JButton("");
-		startBtn.setOpaque(true);
-		startBtn.setContentAreaFilled(false);
-		startBtn.setBorderPainted(true);
-		mainMenuButtonsPane.add(startBtn);
-		startBtn.setBounds(10, 905, 620, 75);
-
-		// Creates and aligns the load button for the main menu.
-		JButton loadBtn = new JButton("");
-		loadBtn.setOpaque(true);
-		loadBtn.setContentAreaFilled(false);
-		loadBtn.setBorderPainted(true);
-		mainMenuButtonsPane.add(loadBtn);
-		loadBtn.setBounds(10, 1003, 570, 75);
-
-		// Creates and aligns the options button for the main menu.
-		JButton optionBtn = new JButton("");
-		optionBtn.setOpaque(true);
-		optionBtn.setContentAreaFilled(false);
-		optionBtn.setBorderPainted(true);
-		mainMenuButtonsPane.add(optionBtn);
-		optionBtn.setBounds(1500, 1003, 410, 75);
-
-		// Creates and aligns the credits button for the main menu.
-		JButton secretBtn = new JButton("");
-		secretBtn.setOpaque(true);
-		secretBtn.setContentAreaFilled(false);
-		secretBtn.setBorderPainted(true);
-		mainMenuButtonsPane.add(secretBtn);
-		secretBtn.setBounds(10, 10, 970, 75);
-
 		// Allows an audio clip to play in the background on the main menu.
 		AudioInputStream music = AudioSystem.getAudioInputStream(new File("resources\\music\\Atmospheric Lab.wav"));
 		AudioFormat format = music.getFormat();
@@ -86,6 +42,102 @@ class primaryGUI extends JFrame {
 		audioClip.open(music);
 		audioClip.start();
 		audioClip.loop(Clip.LOOP_CONTINUOUSLY);
+
+		// Instantiates panels for the mainMenu.
+		JLayeredPane backgroundPane = new JLayeredPane();
+		JLayeredPane mainMenuButtonsPane = new JLayeredPane();
+
+		// Creates a layout manager to handle the organization.
+		OverlayLayout overlayBackgroundPanel = new OverlayLayout(backgroundPane);
+
+		// Sets the layouts for the panels.
+		backgroundPane.setLayout(overlayBackgroundPanel);
+		mainMenuButtonsPane.setLayout(null);
+
+		// Creates a background for the GUI using the assigned photo.
+		JLabel mainMenu = new JLabel("");
+		mainMenu.setMaximumSize(new Dimension(1920, 1080));
+		mainMenu.setIcon(new ImageIcon("resources\\ui\\Main Menu.jpg"));
+		backgroundPane.add(mainMenu);
+
+		// ---------- Start Button ------------ //
+
+		// Creates and aligns the start button for the main menu.
+		startBtn.setOpaque(true);
+		startBtn.setContentAreaFilled(false);
+		startBtn.setBorderPainted(false);
+
+		mainMenuButtonsPane.add(startBtn);
+		startBtn.setBounds(10, 905, 620, 75);
+
+		// The functionality of the startBtn.
+		startBtn.addActionListener(event -> {
+			if(event.getSource() == startBtn){
+				startBtn.setVisible(false);
+				loadBtn.setVisible(false);
+				optionBtn.setVisible(false);
+				secretBtn.setVisible(false);
+
+				mainMenu.setIcon(new ImageIcon("resources\\photos\\introduction\\Intro Sequence 1.jpg"));
+				audioClip.stop();
+
+				AudioInputStream music1 = null;
+				try {
+					music1 = AudioSystem.getAudioInputStream(new File("resources\\music\\Soul Survivor.wav"));
+				} catch (UnsupportedAudioFileException | IOException e) {
+					e.printStackTrace();
+				}
+
+				AudioFormat format1 = music1.getFormat();
+				DataLine.Info info1 = new DataLine.Info(Clip.class, format1);
+				Clip audioClip1 = null;
+				try {
+					audioClip1 = (Clip) AudioSystem.getLine(info1);
+				} catch (LineUnavailableException e) {
+					e.printStackTrace();
+				}
+
+				// Opens and plays the updated music track for the primary game.
+				try {
+					audioClip1.open(music1);
+				} catch (LineUnavailableException | IOException e) {
+					e.printStackTrace();
+				}
+
+				audioClip1.start();
+				audioClip1.loop(Clip.LOOP_CONTINUOUSLY);
+			}
+		});
+
+		// ---------- Load Button ------------ //
+
+		// Creates and aligns the load button for the main menu.
+		loadBtn.setOpaque(true);
+		loadBtn.setContentAreaFilled(false);
+		loadBtn.setBorderPainted(false);
+
+		mainMenuButtonsPane.add(loadBtn);
+		loadBtn.setBounds(10, 1003, 570, 75);
+
+		// ---------- Options Button ------------ //
+
+		// Creates and aligns the options button for the main menu.
+		optionBtn.setOpaque(true);
+		optionBtn.setContentAreaFilled(false);
+		optionBtn.setBorderPainted(false);
+
+		mainMenuButtonsPane.add(optionBtn);
+		optionBtn.setBounds(1500, 1003, 410, 75);
+
+		// ---------- Credits Button ------------ //
+
+		// Creates and aligns the credits button for the main menu.
+		secretBtn.setOpaque(true);
+		secretBtn.setContentAreaFilled(false);
+		secretBtn.setBorderPainted(false);
+
+		mainMenuButtonsPane.add(secretBtn);
+		secretBtn.setBounds(10, 10, 970, 75);
 
 		// Adds the primary JPanels of the GUI.
 		add(mainMenuButtonsPane);
