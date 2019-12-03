e0664bae162539d936138be44c9b9fe73f70e8fa
jacokyle@mail.gvsu.edu <Gv26240614>
2019-12-03 17:54:03 -0500

    Fixed the openable class.

---
+++ src/logicPackage/openable.java
@@ -0,0 +1,40 @@
+package logicPackage;
+
+/***********************************************************************
+ * The door class is used to give a door certain attributes
+ * and functionality.
+ *
+ * @author (Jeremiah Cerriteno, Kyle Jacobson, Austin Jarema)
+ * @version (11/29/19)
+ ***********************************************************************/
+public class openable {
+
+	// For status, 0 is locked and 1 is unlocked.
+	public int status;
+	public String name;
+	public String key;
+	
+	public openable(int status, String name, String key) {
+		this.status = status;
+		this.name = name;
+		this.key = key;
+	}
+
+	private void openActionDenied() {
+		status = 0;
+	}
+
+	private void openActionAccepted() {
+		status = 1;
+	}
+	
+	public void userUnlockAttempt(String currentKey) {
+		if(currentKey.equals(key)) {
+			openActionDenied();
+		}
+
+		else {
+			openActionAccepted();
+		}
+	}
+}
