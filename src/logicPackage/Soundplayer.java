45957120cba08afcb9cf5e7a663555d1342d5436
Kyle Jacobson <jacokyle@mail.gvsu.edu>
2019-12-04 00:58:53 -0500

    Updated comment of the Soundplayer.

--- src/logicPackage/Soundplayer.java
+++ src/logicPackage/Soundplayer.java
@@ -6,8 +6,8 @@ import javax.sound.sampled.Clip;
 import java.io.File;
 
 /***********************************************************************
- * The Soundplayer class allows sound effects to be played while the
- * user interacts with the GUI.
+ * The Soundplayer class allows all non-looping sound effects to be
+ * played while the user interacts with the GUI.
  *
  * @author (Jeremiah Cerriteno, Kyle Jacobson, Austin Jarema)
  * @version (11 / 29 / 19)
